fd6eaad9566a5e1bbbb30ea2b26c84aa4b7ab6d2
Charlie Hulcher <charlie.hulcher@gmail.com>
2016-05-10 15:36:18 -0400

    Update to include android subscriber and source setup bash.

--- run_on_ubilinux_edison_optional_part2.sh
+++ run_on_ubilinux_edison_optional_part2.sh
@@ -22,17 +22,28 @@ on_error () {
 }
 
 ########
-# Start optional setup
-# Sets up tutorials and android sensor subscriber.
-# Not necessary to finish ROS installation
-# ~~~~~Add github url to master project~~~~~
+# Sets up tutorials, common messages, and android sensor subscriber.
 ########
+
 # Add the tutorials
 cd ~/ros_catkin_ws/src && catkin_create_pkg beginner_tutorials std_msgs rospy roscpp || on_error
 cd beginner_tutorials || on_error
 mkdir -p scripts || on_error
-# wget subscriber here
-cd ~/ros_catkin_ws || on_error
-########
-# End optional setup
-########
+
+# Add Android subscriber here
+cd ~/ros_catkin_ws/src/beginner_tutorials/scripts || on_error
+wget https://raw.githubusercontent.com/c-h-/android_subscriber/master/android_subscriber.py
+
+# Add common messages
+cd ~/ros_catkin_ws/src || on_error
+rosws set common_msgs --git https://github.com/ros/common_msgs.git || on_error
+rosws update common_msgs
+cd ~/ros_catkin_ws
+# below line is unnecessary if catkin_make (at the end) runs
+# catkin_make common_msgs
+
+# make everything
+catkin_make
+
+source ~/ros_catkin_ws/devel/setup.bash
+echo "source ~/ros_catkin_ws/devel/setup.bash" >> ~/.bashrc
